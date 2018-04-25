11ddb13dffe4e006f89143e0f57a3ed34979cc0e
DESKTOP-V6DJ1K8\men <beekillua2@gmail.com>
2018-04-25 08:33:58 -0700

    Tugas4-26

---
+++ application/views/home.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Marcus Hansen - UX Designer & Front End Developer</title>
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Favicons
+    ================================================== -->
+<link rel="shortcut icon" href="<?php echo base_url(); ?>assets/img/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" href="<?php echo base_url(); ?>assets/img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url(); ?>assets/img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url(); ?>assets/img/apple-touch-icon-114x114.png">
+
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css"  href="<?php echo base_url(); ?>assets/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/fonts/font-awesome/css/font-awesome.css">
+
+<!-- Stylesheet
+    ================================================== -->
+<link rel="stylesheet" type="text/css"  href="<?php echo base_url(); ?>assets/css/style.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/prettyPhoto.css">
+<link href='http://fonts.googleapis.com/css?family=Lato:400,700,900,300' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/modernizr.custom.js"></script>
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+<!-- Header -->
+<header id="header">
+  <div class="intro text-center">
+    <div class="overlay">
+      <div class="container">
+        <div class="row">
+          <div class="intro-text">
+            <h1>Sultan Alif .M</h1>
+            <p></p>
+            <a href="#about" class="btn btn-default btn-lg page-scroll">Tentang Saya</a> </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+<!-- Navigation -->
+<div id="nav">
+  <nav class="navbar navbar-custom">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse"> <i class="fa fa-bars"></i> </button>
+      </div>
+      
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse navbar-main-collapse">
+        <ul class="nav navbar-nav">
+          <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+          <li class="hidden"> <a href="#page-top"></a> </li>
+          <li> <a class="page-scroll" href="#page-top">Beranda</a> </li>
+          <li> <a class="page-scroll" href="#about">Tentang Saya</a> </li>
+          <li> <a class="page-scroll" href="<?php echo base_url(); ?>index.php/blog/">Blog</a> </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</div>
+<!-- About Section -->
+<div id="about">
+  <div class="container">
+    <div class="section-title text-center center">
+      <h2>Tentang saya</h2>
+      <hr>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-md-6"> <img src="<?php echo base_url(); ?>assets/img/about.jpg" class="img-responsive" alt=""> </div>
+      <div class="col-xs-12 col-md-6">
+        <div class="about-text">
+          <p>Ordinary Boy.</p>
+          <p>born in 1998.</p>
+          <a href="<?php echo base_url(); ?>index.php/blog/" class="btn btn-primary btn-lg page-scroll">My saya</a> </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="footer">
+  <div class="container text-center">
+    <div class="fnav">
+      <p>Copyright &copy; 2016 Marcus Hansen. Designed by <a href="http://www.templatewire.com" rel="nofollow">TemplateWire</a></p>
+    </div>
+  </div>
+</div>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.1.11.1.js"></script> 
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script> 
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/SmoothScroll.js"></script> 
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/easypiechart.js"></script> 
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.prettyPhoto.js"></script> 
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.isotope.js"></script> 
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.counterup.js"></script> 
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/waypoints.js"></script> 
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jqBootstrapValidation.js"></script> 
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/contact_me.js"></script> 
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/main.js"></script>
+</body>
+</html>
---
+++ application/views/news.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hallo! </title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+	<nav class="navbar navbar-default">
+      <div class="container-fluid">
+	<div class="navbar-header">
+		<a class="navbar-brand" href="">WHO!!!</a>
+	</div>
+	<ul class="nav navbar-nav">
+		<li><a href="<?php echo base_url(); ?>">Home</a></li>
+		<li><a href="<?php echo base_url('index.php\Welcome\about'); ?>">About</a></li>
+		<li><a href="<?php echo base_url('index.php\blog'); ?>">Blog</a></li> 
+		 
+	</ul>
+   </div>
+</nav>
+<div class="jumbotron">
+		<h2>Sedikit Tentang saya :</h2>
+		<h2>Nama : Sultan Alif</h2>
+		<h2>Kelas : MI-2C</h2>
+		<h2>Kuliah : Politeknik Negeri Malang</h2>
+		<h2>Hobby : Game</h2>
+	</div>
+</body>
+
+</html>
